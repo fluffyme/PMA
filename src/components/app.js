d40646e9ad6fc265b9bb55056e72661bdb67bb80
Ana Lisnic <ana.lisnnic@gmail.com>
2018-08-02 19:03:01 +0300

    initialised project

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Property Management Application</h1>
+      </div>
+    );
+  }
+}
